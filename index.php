3f2b68e93b3d92dfcfcd60b4b9ae91200d642a57
sofilarrea <sofialejandra@gmail.com>
2023-09-19 19:27:36 -0300

    myqli

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 </head>
 <body>
 
-  <h1>la re concha de la lora</h1>
-
+  <h1>la re concha de la muerte</h1>
+  <?php $conn = mysqli_connect('localhost', 'root',' ','blogs'); ?>
 
 <!--Bootstrap Script -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
